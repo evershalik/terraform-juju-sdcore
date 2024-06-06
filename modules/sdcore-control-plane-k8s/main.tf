a1e1e25a7e4fb1b4210fa577c58de12245461f31
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2024-06-06 08:33:19 +0200

    chore: Adds sdcore_config integration for NRF and SMF (#47)

--- modules/sdcore-control-plane-k8s/main.tf
+++ modules/sdcore-control-plane-k8s/main.tf
@@ -334,6 +334,34 @@ resource "juju_integration" "ausf-sdcore-config" {
   }
 }
 
+resource "juju_integration" "nrf-sdcore-config" {
+  model = var.create_model == true ? juju_model.sdcore[0].name : var.model_name
+
+  application {
+    name     = module.nrf.app_name
+    endpoint = module.nrf.sdcore_config_endpoint
+  }
+
+  application {
+    name     = module.webui.app_name
+    endpoint = module.webui.sdcore_config_endpoint
+  }
+}
+
+resource "juju_integration" "smf-sdcore-config" {
+  model = var.create_model == true ? juju_model.sdcore[0].name : var.model_name
+
+  application {
+    name     = module.smf.app_name
+    endpoint = module.smf.sdcore_config_endpoint
+  }
+
+  application {
+    name     = module.webui.app_name
+    endpoint = module.webui.sdcore_config_endpoint
+  }
+}
+
 # Integrations for `metrics` endpoint
 
 resource "juju_integration" "amf-metrics" {
